4ceccc7ec7a5d854ea8ca8e6b689628666cf855a
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2020-12-28 15:08:05 +0100

    refactor code

--- index.js
+++ index.js
@@ -37,57 +37,8 @@
 //   });
 // }
 
-
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
-for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    var buttonLetter = this.innerHTML;
-    switch (buttonLetter) {
-      case "w":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-
-      case "a":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-        break;
-
-      case "s":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-        break;
-
-      case "d":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-        break;
-
-      case "j":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-
-      case "k":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-
-      case "l":
-        var audio = new Audio("sounds/gothic.mp3");
-        audio.play();
-        break;
-
-      default:
-        console.log("Something went wrong.");
-
-    }
-  });
-}
-
-document.addEventListener("keydown", eventlistener => {
-  switch (eventlistener.key) {
+function makeSound(char) {
+  switch (char) {
     case "w":
       var audio = new Audio("sounds/tom-1.mp3");
       audio.play();
@@ -126,4 +77,22 @@ document.addEventListener("keydown", eventlistener => {
     default:
       console.log("Something went wrong.");
   }
+}
+
+// Detecting button press
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonLetter = this.innerHTML;
+    makeSound(buttonLetter);
+  });
+}
+
+// Detecting keyboard press
+
+document.addEventListener("keydown", eventlistener => {
+  var pressedKey = eventlistener.key;
+  makeSound(pressedKey);
 });
